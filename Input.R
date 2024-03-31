c61a8bd6751a8b16443b04e0ef2c1f706a50d4f8
Amit Kushwaha <145186713+Amit72777@users.noreply.github.com>
2024-03-31 22:11:58 +0530

    Introduction of taking input in R language

---
+++ Input.R
@@ -0,0 +1,13 @@
+name <- readline(prompt = "Enter your name ")
+print(name)
+
+age <- readline(prompt = "Enter your age:\n") #take input as a string 
+age<- as.integer(age) # convert into integer 
+
+print(age)
+
+gender <-readline(prompt = "enter you gender:")
+
+print(gender)
+
+
